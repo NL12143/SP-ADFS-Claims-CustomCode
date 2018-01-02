93a72773be4f01ea8782bedd9d145106456da1ad
SharePointSan <NL12143@hotmail.com>
2018-01-02 16:14:03 +0100

    Create SingleSignoutModule.cs

---
+++ SingleSignoutModule.cs
@@ -0,0 +1,33 @@
+In order to use the HttpModules, the web.config of the SharePoint site has to be updated.
+Usually, the web.config of the SharePoint applications are placed in the following location:
+C:\inetpub\wwwroot\wss\VirtualDirectories\XXXXX\web.config
+[where XXXXX is the port that application is running on]
+
+### Single Sign Out module    ------
+Add the following elements to the web.config of your application.
+Under system.web:
+  <system.web>
+    <httpModules>
+      <add name="SingleSignOut" type="SingleSignOutModule.SingleSignOutModule, SingleSignOutModule, Version=1.0.0.0, Culture=neutral, PublicKeyToken=996db41c32d1031c" />
+Under system.webServer
+  <system.webServer>
+    <modules ...>
+      <add name="SingleSignOut" type="SingleSignOutModule.SingleSignOutModule, SingleSignOutModule, Version=1.0.0.0, Culture=neutral, PublicKeyToken=996db41c32d1031c" />
+
+### Sliding Session module    ------
+Add the following elements to the web.config of your application.
+Under system.web:
+  <system.web>
+    <httpModules>
+      <add name="SlidingSession" type="SlidingSessionModule.SlidingSessionModule, SlidingSessionModule, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b61de93f440f208f" />
+Under system.webServer
+  <system.webServer>
+    <modules ...>
+      <add name="SlidingSession" type="SlidingSessionModule.SlidingSessionModule, SlidingSessionModule, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b61de93f440f208f" />
+
+### WHR module    ------
+Under system.webServer
+  <system.webServer>
+    <modules runAllManagedModulesForAllRequests="true">
+      <add name="SPWHRModule" type="SP.Site.HttpModules.AddWHR, AG.Inside, Version=1.0.0.0, Culture=neutral, PublicKeyToken=112ea9c270aaa72a" />
+
